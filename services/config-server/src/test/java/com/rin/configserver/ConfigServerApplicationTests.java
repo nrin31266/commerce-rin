e445b0682c09cc2e0c554348208ee9478b215aed
nrin31266 <162881731+nrin31266@users.noreply.github.com>
2024-08-17 20:13:45 +0700

    f

---
+++ services/config-server/src/test/java/com/rin/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rin.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
